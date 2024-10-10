7ef4622ad316b69720a55d6980bcfa6eddfe679a
Osvaldo Dias Junior <160083489+Osvaldo065@users.noreply.github.com>
2024-10-10 09:35:29 -0400

    Inserindo um BD
    Estou inserindo os Scripts de um pequeno banco de dados que criei. 
    
    Realizada a criação de algumas Querys, será possível visualizar no final dos Scripts.

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,30 @@
+-- TABLE
+CREATE TABLE Cliente(
+    ClienteId INT NOT NULL,
+    NomeCliente VARCHAR(70) NOT NULL,
+    Ativo VARCHAR(2) NOT NULL,
+    PRIMARY KEY (ClienteId)
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Produto(
+    ProdutoId INT NOT NULL,
+    NomeProduto VARCHAR(90) NOT NULL,
+    PRIMARY KEY (ProdutoId)
+);
+CREATE TABLE Venda(
+    VendaId INT NOT NULL,
+    ProdutoId INT NOT NULL,
+    ClienteId INT NOT NULL,
+    Valor DECIMAL NOT NULL,
+    DataVenda DATE NOT NULL,
+    PRIMARY KEY (VendaId),
+    FOREIGN KEY (ProdutoId) REFERENCES Produto(ProdutoId),
+    FOREIGN KEY (ClienteId) REFERENCES Cliente(ClienteId)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
